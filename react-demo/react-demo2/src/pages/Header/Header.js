f490e222ab3497d9962f9d0cff235f1ffa09d23c
qwe3192359 <635869472@qq.com>
2018-04-19 18:51:28 +0800

    create-react-app建立react项目

---
+++ react-demo/react-demo2/src/pages/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+        <header className="header">
+          <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+          </ul>
+        </header>
+    );
+  }
+}
+export default Header;
