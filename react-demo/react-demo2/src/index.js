45e81e435e8847ddaa49740fb8f7eb2c7ffb40f9
qwe3192359 <635869472@qq.com>
2018-04-20 21:57:10 +0800

    使用react做实际项目

--- react-demo/react-demo2/src/index.js
+++ react-demo/react-demo2/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './pages/Header/Header';
+import Home from './pages/Home/Home';
+import  './style/reset.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
---
+++ react-demo/react-demo2/src/pages/Home/Home.js
@@ -0,0 +1,29 @@
+/**
+ * Created by yy on 2018/4/20.
+ */
+import React, {Component} from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div className="head">
+                    <div>
+                        菜单按钮
+                    </div>
+                    <div>
+                        5月
+                    </div>
+                    <div>
+                        今天
+                    </div>
+                </div>
+                <div className="date">
+
+                </div>
+            </div>
+        );
+    }
+}
+export default Home;
