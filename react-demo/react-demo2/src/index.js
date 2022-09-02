58646f585e085ae6f6239eb432d68e099c349bfa
yyf <635869472@qq.com>
2022-09-02 08:32:05 +0800

    react-demo

--- react-demo/react-demo2/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Home from './pages/Home/Home';
-import  './style/reset.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
